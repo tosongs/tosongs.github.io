9fa7eb3761cdc49f2a31cff757ec14b4db0ddd5e
tosongs <47690764+tosongs@users.noreply.github.com>
2019-02-25 15:08:59 +0530

    Add files via upload

---
+++ more.js
@@ -0,0 +1,865 @@
+ var i=0;
+
+  function first()
+  {
+  
+    if(i==0)
+    {
+      $("#change").css("color","#1abc9c");
+      $(".boxes").css("background","#1abc9c");
+    	i=i+1;
+    	console.log(i);
+    }
+    else 
+    {
+        $("#change").css("color","#e74c3c");
+    	i=i-1;
+    	console.log(i);
+    }  
+    $(".audio,.load,.boxes").toggle();
+  }
+
+    function second()
+  {
+
+  	
+    if(i==0)
+    {
+      $("#change1").css("color","#3498db");
+      $(".boxes").css("background","#3498db");
+    	i=i+1;
+    	console.log(i);
+    }
+    else 
+    {
+        $("#change1").css("color","#e74c3c");
+    	i=i-1;
+    	console.log(i);
+    } 
+    $(".audio1,.load,.boxes").toggle();
+    
+  }
+
+    function thrid()
+  {
+  
+    if(i==0)
+    {
+      $("#change2").css("color","#9b59b6");
+      $(".boxes").css("background","#9b59b6");
+    	i=i+1;
+    	console.log(i);
+    }
+    else 
+    {
+        $("#change2").css("color","#e74c3c");
+    	i=i-1;
+    	console.log(i);
+    } 
+    $(".audio2,.load,.boxes").toggle();
+     
+  }
+
+    function four()
+  {
+  	
+    if(i==0)
+    {
+      $("#change3").css("color","#34495e");
+      $(".boxes").css("background","#34495e");
+    	i=i+1;
+    	console.log(i);
+    }
+    else 
+    {
+        $("#change3").css("color","#e74c3c");
+    	i=i-1;
+    	console.log(i);
+    } 
+    $(".audio3,.load,.boxes").toggle();
+    
+  }
+
+      function five()
+  {
+  	 	
+    if(i==0)
+    {
+      $("#change4").css("color","#95a5a6");
+      $(".boxes").css("background","#95a5a6");
+    	i=i+1;
+    	console.log(i);
+    }
+    else 
+    {
+        $("#change4").css("color","#e74c3c");
+    	i=i-1;
+    	console.log(i);
+    } 
+    $(".audio4,.load,.boxes").toggle();
+    
+  }
+
+    function six()
+  {
+  	 
+  	console.log(i);
+    if(i==0)
+    {
+      $("#change5").css("color","#e67e22");
+      $(".boxes").css("background","#e67e22");
+    	i=i+1;
+    	console.log(i);
+    }
+    else 
+    {
+        $("#change5").css("color","#e74c3c");
+    	i=i-1;
+    	console.log(i);
+    } 
+    $(".audio5,.load,.boxes").toggle();
+    
+  }
+
+    function seven()
+  {
+  	 
+    if(i==0)
+    {
+      $("#change6").css("color","#C4E538");
+      $(".boxes").css("background","#C4E538");
+    	i=i+1;
+    	console.log(i);
+    }
+    else 
+    {
+        $("#change6").css("color","#e74c3c");
+    	i=i-1;
+    	console.log(i);
+    } 
+    $(".audio6,.load,.boxes").toggle();
+    
+  }
+  
+    function hello()
+  {
+  	 
+    if(i==0)
+    {
+      $("#change7").css("color","#d35400");
+      $(".boxes").css("background","#d35400");
+    	i=i+1;
+    	console.log(i);
+    }
+    else 
+    {
+        $("#change7").css("color","#e74c3c");
+    	i=i-1;
+    	console.log(i);
+    } 
+    $(".audio7,.load,.boxes").toggle();
+    
+  }
+  
+    function nine()
+  {
+  	
+    if(i==0)
+    {
+      $("#change8").css("color","#bdc3c7");
+      $(".boxes").css("background","#bdc3c7");
+    	i=i+1;
+    	console.log(i);
+    }
+    else 
+    {
+        $("#change8").css("color","#e74c3c");
+    	i=i-1;
+    	console.log(i);
+    } 
+    $(".audio8,.load,.boxes").toggle();
+     
+  }
+
+    function ten()
+  {
+  	 
+    if(i==0)
+    {
+      $("#change9").css("color","#FDA7DF");
+      $(".boxes").css("background","#FDA7DF");
+    	i=i+1;
+    	console.log(i);
+    }
+    else 
+    {
+        $("#change9").css("color","#e74c3c");
+    	i=i-1;
+    	console.log(i);
+    } 
+    $(".audio9,.load,.boxes").toggle();
+     
+  }
+
+      function starw()
+  {
+  	
+    if(i==0)
+    {
+      $("#change10").css("color","#B53471");
+      $(".boxes").css("background","#B53471");
+    	i=i+1;
+    	console.log(i);
+    }
+    else 
+    {
+        $("#change10").css("color","#e74c3c");
+    	i=i-1;
+    	console.log(i);
+    } 
+    $(".audio10,.load,.boxes").toggle();
+    
+  }
+
+ 	function pon()
+ 	{
+ 	
+    if(i==0)
+    {
+      $("#change11").css("color","#12CBC4");
+      $(".boxes").css("background","#12CBC4");
+    	i=i+1;
+    	console.log(i);
+    }
+    else 
+    {
+        $("#change11").css("color","#e74c3c");
+    	i=i-1;
+    	console.log(i);
+    } 
+ 		$(".audio11,.load,.boxes").toggle();
+ 		
+ 	}
+
+ 	 	function att()
+ 	{
+ 	
+    if(i==0)
+    {
+      $("#change12").css("color","#f6e58d");
+      $(".boxes").css("background","#f6e58d");
+    	i=i+1;
+    	console.log(i);
+    }
+    else 
+    {
+        $("#change12").css("color","#e74c3c");
+    	i=i-1;
+    	console.log(i);
+    } 
+ 		$(".audio12,.load,.boxes").toggle();
+ 		
+ 	}
+ 		 	
+ 		 	function djs()
+ 	{
+ 	
+    if(i==0)
+    {
+      $("#change13").css("color","#ff7979");
+      $(".boxes").css("background","#ff7979");
+    	i=i+1;
+    	console.log(i);
+    }
+    else 
+    {
+        $("#change13").css("color","#e74c3c");
+    	i=i-1;
+    	console.log(i);
+    } 
+ 		$(".audio13,.load,.boxes").toggle();
+ 		
+ 	}
+
+    	function eth()
+ 	{
+ 	
+    if(i==0)
+    {
+      $("#change14").css("color","#badc58");
+      $(".boxes").css("background","#badc58");
+    	i=i+1;
+    	console.log(i);
+    }
+    else 
+    {
+        $("#change14").css("color","#e74c3c");
+    	i=i-1;
+    	console.log(i);
+    } 
+ 		$(".audio14,.load,.boxes").toggle();
+ 		
+ 	} 
+
+      function ibl()
+  {
+  
+    if(i==0)
+    {
+      $("#change15").css("color","#f9ca24");
+      $(".boxes").css("background","#f9ca24");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change15").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio15,.load,.boxes").toggle();
+    
+  } 
+
+
+       function imo()
+  {
+  
+    if(i==0)
+    {
+      $("#change16").css("color","#eb4d4b");
+      $(".boxes").css("background","#eb4d4b");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change16").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio16,.load,.boxes").toggle();
+    
+  } 
+
+       function ira()
+  {
+  
+    if(i==0)
+    {
+      $("#change17").css("color","#95afc0");
+      $(".boxes").css("background","#95afc0");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change17").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio17,.load,.boxes").toggle();
+    
+  } 
+
+
+        function ith()
+  {
+  
+    if(i==0)
+    {
+      $("#change18").css("color","#4834d4");
+      $(".boxes").css("background","#4834d4");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change18").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio18,.load,.boxes").toggle();
+    
+  } 
+
+
+         function kad()
+  {
+  
+    if(i==0)
+    {
+      $("#change19").css("color","#130f40");
+      $(".boxes").css("background","#130f40");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change19").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio19,.load,.boxes").toggle();
+    
+  } 
+
+           function kmo()
+  {
+  
+    if(i==0)
+    {
+      $("#change20").css("color","#fdcb6e");
+      $(".boxes").css("background","#fdcb6e");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change20").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio20,.load,.boxes").toggle();
+    
+  } 
+
+             function kne()
+  {
+  
+    if(i==0)
+    {
+      $("#change21").css("color","#ff7675");
+      $(".boxes").css("background","#ff7675");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change21").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio21,.load,.boxes").toggle();
+    
+  } 
+
+              function kon()
+  {
+  
+    if(i==0)
+    {
+      $("#change22").css("color","#6c5ce7");
+      $(".boxes").css("background","#6c5ce7");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change22").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio22,.load,.boxes").toggle();
+    
+  } 
+
+   function naa()
+  {
+  
+    if(i==0)
+    {
+      $("#change23").css("color","#e17055");
+      $(".boxes").css("background","#e17055");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change23").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio23,.load,.boxes").toggle();
+    
+  } 
+
+     function nev()
+  {
+  
+    if(i==0)
+    {
+      $("#change24").css("color","#487eb0");
+      $(".boxes").css("background","#487eb0");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change24").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio24,.load,.boxes").toggle();
+    
+  } 
+
+      function news()
+  {
+  
+    if(i==0)
+    {
+      $("#change25").css("color","#4cd137");
+      $(".boxes").css("background","#4cd137");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change25").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio25,.load,.boxes").toggle();
+    
+  }
+
+        function ohp()
+  {
+  
+    if(i==0)
+    {
+      $("#change26").css("color","#fbc531");
+      $(".boxes").css("background","#fbc531");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change26").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio26,.load,.boxes").toggle();
+    
+  }
+
+          function oru()
+  {
+  
+    if(i==0)
+    {
+      $("#change27").css("color","#e84118");
+      $(".boxes").css("background","#e84118");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change27").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio27,.load,.boxes").toggle();
+    
+  }
+
+            function osa()
+  {
+  
+    if(i==0)
+    {
+      $("#change28").css("color","#273c75");
+      $(".boxes").css("background","#273c75");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change28").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio28,.load,.boxes").toggle();
+    
+  }
+
+              function poi()
+  {
+  
+    if(i==0)
+    {
+      $("#change29").css("color","#EAB543");
+      $(".boxes").css("background","#EAB543");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change29").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio29,.load,.boxes").toggle();
+    
+  }
+
+               function poe()
+  {
+  
+    if(i==0)
+    {
+      $("#change30").css("color","#B33771");
+      $(".boxes").css("background","#B33771");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change30").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio30,.load,.boxes").toggle();
+    
+  }
+
+                function sib()
+  {
+  
+    if(i==0)
+    {
+      $("#change31").css("color","#FD7272");
+      $(".boxes").css("background","#FD7272");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change31").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio31,.load,.boxes").toggle();
+    
+  }
+
+                function tsk()
+  {
+  
+    if(i==0)
+    {
+      $("#change32").css("color","#82589F");
+      $(".boxes").css("background","#82589F");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change32").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio32,.load,.boxes").toggle();
+    
+  }
+
+                  function tha()
+  {
+  
+    if(i==0)
+    {
+      $("#change33").css("color","#4b4b4b");
+      $(".boxes").css("background","#4b4b4b");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change33").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio33,.load,.boxes").toggle();
+    
+  }
+
+                    function tenw()
+  {
+  
+    if(i==0)
+    {
+      $("#change34").css("color","#34ace0");
+      $(".boxes").css("background","#34ace0");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change34").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio34,.load,.boxes").toggle();
+    
+  }
+
+     function upn()
+  {
+  
+    if(i==0)
+    {
+      $("#change35").css("color","#3ae374");
+      $(".boxes").css("background","#3ae374");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change35").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio35,.load,.boxes").toggle();
+    
+  }
+
+    function unv()
+  {
+  
+    if(i==0)
+    {
+      $("#change36").css("color","#ffaf40");
+      $(".boxes").css("background","#ffaf40");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change36").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio36,.load,.boxes").toggle();
+    
+  }
+
+      function vpv()
+  {
+  
+    if(i==0)
+    {
+      $("#change37").css("color","#3d3d3d");
+      $(".boxes").css("background","#3d3d3d");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change37").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio37,.load,.boxes").toggle();
+    
+  }
+
+     function vae()
+  {
+  
+    if(i==0)
+    {
+      $("#change38").css("color","#ff3838");
+      $(".boxes").css("background","#ff3838");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change38").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio38,.load,.boxes").toggle();
+    
+  }
+
+   function vnp()
+  {
+  
+    if(i==0)
+    {
+      $("#change39").css("color","#218c74");
+      $(".boxes").css("background","#218c74");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change39").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio39,.load,.boxes").toggle();
+    
+  }
+
+        function vos()
+  {
+  
+    if(i==0)
+    {
+      $("#change40").css("color","#ff5252");
+      $(".boxes").css("background","#ff5252");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change40").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio40,.load,.boxes").toggle();
+    
+  }
+
+       function vvy()
+  {
+  
+    if(i==0)
+    {
+      $("#change41").css("color","#ffb142");
+      $(".boxes").css("background","#ffb142");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change41").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio41,.load,.boxes").toggle();
+    
+  }
+
+       function yay()
+  {
+  
+    if(i==0)
+    {
+      $("#change42").css("color","#ccae62");
+      $(".boxes").css("background","#ccae62");
+      i=i+1;
+      console.log(i);
+    }
+    else 
+    {
+        $("#change42").css("color","#e74c3c");
+      i=i-1;
+      console.log(i);
+    } 
+    $(".audio42,.load,.boxes").toggle();
+    
+  }
